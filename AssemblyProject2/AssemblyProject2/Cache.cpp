fe693849c656602f179f3ef6b96fbe17e61ec0ef
saifsabry21 <saif-sabry@aucegypt.edu>
2025-05-14 00:09:32 +0300

    Finished Experiment 1 implementation in Main for FACacheSim

--- AssemblyProject2/AssemblyProject2/Cache.cpp
+++ AssemblyProject2/AssemblyProject2/Cache.cpp
@@ -1,11 +1,21 @@
-#include <iostream>
+﻿#include <iostream>
 #include <iomanip>
 #include <vector>
 using namespace std;
 #define DBG 1
 #define DRAM_SIZE (64*1024*1024)
 #define CACHE_SIZE (64*1024)
+struct CacheLineFA {
+	unsigned int tag = 0;
+	bool valid = false;
+	unsigned int leastrecentuseCounter = 0;
+};
 enum cacheResType { MISS = 0, HIT = 1 };
+static unsigned int   FaLineSize = 64;
+static unsigned int FaCacheLines = CACHE_SIZE / FaLineSize;
+static CacheLineFA* FaCache = new CacheLineFA[FaCacheLines]{};
+static unsigned int   FaAccessCount = 0;
+
 /* The following implements a random number generator */
 unsigned int m_w = 0xABABAB55; /* must not be zero, nor 0x464fffff */
 unsigned int m_z = 0x05080902; /* must not be zero, nor 0x9068ffff */
@@ -53,18 +63,14 @@ cacheResType cacheSimDM(unsigned int addr)
 	//transaction is a miss
 		return MISS;
 }
-struct CacheLineFA {
-	unsigned int tag = 0;
-	bool valid = false;
-	unsigned int leastrecentuseCounter = 0;
-};
+
 // Fully Associative Cache Simulator
 cacheResType cacheSimFA(unsigned int addr)
 {
-	static unsigned int lineSize = 64;
-	static unsigned int cacheLines = CACHE_SIZE / lineSize;
-	static CacheLineFA* cache = new CacheLineFA[cacheLines]{};
-	static unsigned int accessCount = 0;
+	unsigned int lineSize = FaLineSize;
+	unsigned int cacheLines = FaCacheLines;
+	CacheLineFA* cache = FaCache;
+	unsigned int& accessCount = FaAccessCount;
 
 	unsigned int tag = addr / lineSize;
 	// Check for hit
@@ -102,23 +108,53 @@ cacheResType cacheSimFA(unsigned int addr)
 	cache[lruIndex].leastrecentuseCounter = ++accessCount;
 		return MISS;
 }
+// Re‑initialize the FA cache for new line size
+void resetFACache(unsigned int newLineSize) {
+	FaLineSize = newLineSize;
+	FaCacheLines = CACHE_SIZE / FaLineSize;
+	FaAccessCount = 0;
+
+	delete[] FaCache;
+	FaCache = new CacheLineFA[FaCacheLines]();
+}
+
+
 const char* msg[2] = { "Miss","Hit" };
 #define NO_OF_Iterations 100 // CHange to 1,000,000
 int main()
 {
-	unsigned int hit = 0;
-	cacheResType r;
-	unsigned int addr;
-	cout << "Direct Mapped Cache Simulator\n";
-	for (int inst = 0; inst < NO_OF_Iterations; inst++)
-	{
-		addr = memGen2();
-		r = cacheSimDM(addr);
-		if (r == HIT) hit++;
-		cout << "0x" << setfill('0') << setw(8) << hex << addr << " (" << msg[r]
-			<< ")\n";
+	const unsigned int sizes[] = { 16, 32, 64, 128 };
+	const unsigned int SetNum = 4;
+
+	cout << "Experiment 1 for FACacheSim: 4 sets, varying line size"<<endl;
+
+	for (unsigned int L : sizes) {
+		resetFACache(L);
+
+	unsigned int totalLines = CACHE_SIZE / L;
+	unsigned int ways = totalLines / SetNum;
+
+		cout << "Line size = " << L << " bytes, Ways = " << ways << "\n";
+		//cout << "Address (Miss/Hit)\n";
+
+		unsigned long long hits = 0;
+		for (unsigned long long i = 0; i < NO_OF_Iterations; ++i) {
+			unsigned int addr = memGen2();
+			cacheResType r = cacheSimFA(addr);
+			if (r == HIT) {
+				hits++;
+			}
+
+			//cout << "0x"
+				//<< setfill('0') << setw(8) << hex << addr
+				//<< " (" << msg[r] << ")\n";
+		}
+		double ratio = 100.0 * hits / NO_OF_Iterations;
+		cout << dec << "Hit ratio = " << fixed << setprecision(3) << ratio << "%" << endl;
 	}
-	cout << "Hit ratio = " << (100 * hit / NO_OF_Iterations) << endl;
+	cout << endl;
+
+	// cleanup
+	delete[] FaCache;
 	return 0;
 }
-
